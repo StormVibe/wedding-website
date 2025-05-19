8c860b0ad7005e6a1f3982a0e7e0794e752e52fe
StormVibe <divna.nick.wedding@gmail.com>
2025-05-19 18:57:03 -0400

    Revamping website + added blog - TO DO: Add mailing list api

--- js/scripts.js
+++ js/scripts.js
@@ -164,20 +164,20 @@ $(document).ready(function () {
             title: "Nick and Divna's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jan 11, 2026 15:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jan 11, 2026 16:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '2219 Summit St Columbus, Ohio',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. Please subscribe to our mailing list"
         }
     });
 
